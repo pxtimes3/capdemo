656e9f5645592527b7d6e2dbce174a19b43860f5
root <root@ip-172-31-17-160.eu-central-1.compute.internal>
2015-12-09 12:55:01 +0000

    parameters

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <html>
 <body>
 
@@ -12,6 +11,16 @@ file_get_contents("http://instance-data/latest/meta-data/instance-id");
         print("Instance ID: $instance_id");
 ?>
 
+<div>
+<div>
+
+<?php
+	$line = file_get_contents("../configfile");
+	$creds = explode(" ",$line);
+	echo $creds[0];
+	echo $creds[1];
+	echo $creds[2];
+?>
 
 </body>
 </html>
